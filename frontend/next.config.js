a6b551df8dfd20dfca610f5c49f0afbf2da66b26
Hendrik Pauthner <pauthner@campus.tu-berlin.de>
2026-01-08 22:37:06 +0100

    Add Next.js configuration to save memory for deploymant, advances #18

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 1. Disable source maps to save memory
+  productionBrowserSourceMaps: false,
+  
+  // 2. Reduce Image Optimization cache (Optional but helps)
+  images: {
+    unoptimized: true, 
+  },
+};
 
 module.exports = nextConfig;
